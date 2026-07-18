505c49f6debddddb5d1480f93d98d718d84ececf
Longvu4705 <huyenlongmai@gmail.com>
2026-07-18 08:38:26 +0700

    Add files via upload

---
+++ do_an_2_dg/proj/do_an_2.c
@@ -0,0 +1,17 @@
+/*
+ * do_an_2.c
+ *
+ * Created: 5/21/2026 2:26:05 PM
+ * Author: admin
+ */
+
+#include <mega16.h>
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
